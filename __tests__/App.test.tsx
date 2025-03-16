a0d282d0cffd607565a94cd0bdc278f913fb015c
emilyb93 <emilybennett93@hotmail.com>
2025-03-16 14:16:47 +0000

    fix: updating test title

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, screen } from "@testing-library/react";
 import App from "../src/App";
-test("renders three elements with the class of .pricing-card", async () => {
+test("renders three plan titles to the page", async () => {
   render(<App />);
 
   await screen.findByText("Free");
